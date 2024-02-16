9075b69262c1787d369a5063386127760ace82b9
Zidan0103 <zidan.zidan41@hotmail.com>
2024-02-16 14:29:43 +0100

    ändring Z

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,11 +25,5 @@ const count = ref(0)
       >create-vue</a
     >, the official Vue + Vite starter
   </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+<!-- ändring -->
 </template>
-<!-- hejjjjjjjjjjj -->
