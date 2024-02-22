e39b637360c6960c0c041bc06a97adde41232289
filip johanson <Filip.johansson@iths.se>
2024-02-22 15:26:37 +0100

    ändringar i login fliken

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+
+<template>
+
+      <BNavItemDropdown right>
+        <!-- Using 'button-content' slot -->
+        <template #button-content>
+          <em>User</em>
+        </template>
+        <div id="LogOut">
+          <BDropdownItem href="#">Register</BDropdownItem>
+          <BDropdownItem href="#">Sign In</BDropdownItem>
+        </div>
+        <div id="LogIn">
+          <BDropdownItem href="#">Profile</BDropdownItem>
+          <BDropdownItem href="#">Sign Out</BDropdownItem>
+        </div>
+      </BNavItemDropdown>
+</template>
+
+
+<script>
+import { RouterLink } from 'vue-router';
+</script>
+
+
+<style scoped>
+div{
+  height: 10vh;
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,49 +1,5 @@
-
-<template>
-
-
-<BNavbar toggleable="lg" variant="primary" v-b-color-mode="'dark'">
-
-
-  <!-- <BNavbarBrand href="#">NavBar</BNavbarBrand> -->
-
-  <BNavbarToggle target="nav-collapse" />
-  <BCollapse id="nav-collapse" is-nav>
-    <BNavbarNav>
-      <RouterLink class="nav-link"  to="/">Home</RouterLink>
-      <RouterLink class="nav-link"  to="/contact">Contact</RouterLink>
-      <RouterLink class="nav-link"  to="/about">About</RouterLink>
-
-    </BNavbarNav>
-    <!-- Right aligned nav items -->
-    <BNavbarNav class="ms-auto mb-2 mb-lg-0">
-      <BNavItemDropdown text="Lang" right>
-        <BDropdownItem href="#">EN</BDropdownItem>
-        <BDropdownItem href="#">ES</BDropdownItem>
-        <BDropdownItem href="#">RU</BDropdownItem>
-        <BDropdownItem href="#">FA</BDropdownItem>
-      </BNavItemDropdown>
-      <BNavItemDropdown right>
-        <!-- Using 'button-content' slot -->
-        <template #button-content>
-          <em>User</em>
-        </template>
-        <BDropdownItem href="#">Profile</BDropdownItem>
-        <BDropdownItem href="#">Sign Out</BDropdownItem>
-      </BNavItemDropdown>
-    </BNavbarNav>
-    <BNavForm class="d-flex">
-      <BFormInput class="me-2" placeholder="Search" />
-      <BButton variant="outline-warning">Search</BButton>
-    </BNavForm>
-  </BCollapse>
-</BNavbar>
-
-</template>
-
-
 <script>
-
+  import Login from '../components/Login.vue'
 import { RouterLink } from 'vue-router';
 
   export default {
@@ -70,6 +26,44 @@ import { RouterLink } from 'vue-router';
     }
   }
 </script>
+
+<template>
+
+
+<BNavbar toggleable="lg" variant="primary" v-b-color-mode="'dark'">
+
+
+  <!-- <BNavbarBrand href="#">NavBar</BNavbarBrand> -->
+
+  <BNavbarToggle target="nav-collapse" />
+  <BCollapse id="nav-collapse" is-nav>
+    <BNavbarNav>
+      <RouterLink class="nav-link"  to="/">Home</RouterLink>
+      <RouterLink class="nav-link"  to="/contact">Contact</RouterLink>
+      <RouterLink class="nav-link"  to="/about">About</RouterLink>
+
+    </BNavbarNav>
+    <!-- Right aligned nav items -->
+    <BNavbarNav class="ms-auto mb-2 mb-lg-0">
+      <BNavItemDropdown text="Lang" right>
+        <BDropdownItem href="#">EN</BDropdownItem>
+        <BDropdownItem href="#">ES</BDropdownItem>
+        <BDropdownItem href="#">RU</BDropdownItem>
+        <BDropdownItem href="#">FA</BDropdownItem>
+      </BNavItemDropdown>
+      <Login/>
+    </BNavbarNav>
+    <BNavForm class="d-flex">
+      <BFormInput class="me-2" placeholder="Search" />
+      <BButton variant="outline-warning">Search</BButton>
+    </BNavForm>
+  </BCollapse>
+</BNavbar>
+
+</template>
+
+
+
 
 
 <style scoped>
